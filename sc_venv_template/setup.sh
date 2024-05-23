145798b4be3b8c6f76b87ef0ff9b9806a47dfaad
xhagrg <grg.iksha@hotmail.com>
2024-05-23 15:12:47 -0500

    Use symlinks and install proper packages.

--- sc_venv_template/setup.sh
+++ sc_venv_template/setup.sh
@@ -5,6 +5,7 @@ SOURCE_PATH="${BASH_SOURCE[0]:-${(%):-%x}}"
 RELATIVE_PATH="$(dirname "$SOURCE_PATH")"
 ABSOLUTE_PATH="$(realpath "${RELATIVE_PATH}")"
 
+source "${ABSOLUTE_PATH}"/symlink.sh
 source "${ABSOLUTE_PATH}"/config.sh
 source "${ABSOLUTE_PATH}"/modules.sh
 
@@ -13,3 +14,5 @@ python3 -m venv --prompt "$ENV_NAME" --system-site-packages "${ENV_DIR}"
 source "${ABSOLUTE_PATH}"/activate.sh
 
 python3 -m pip install --upgrade -r "${ABSOLUTE_PATH}"/requirements.txt
+mim install mmcv-full==1.6.2 -f https://download.openmmlab.com/mmcv/dist/cu115/torch1.11.0/index.html
+pip install git+https://github.com/NASA-IMPACT/hls-foundation-os.git
