eb0f2c67475bc0277b688faee1e014ec2eca3883
Alexandre Strube <a.strube@fz-juelich.de>
2024-05-23 17:50:29 +0200

    Venv on hpc

---
+++ sc_venv_template/modules.sh
@@ -0,0 +1,10 @@
+module purge
+module load Stages/2023
+module load GCC OpenMPI
+# Some base modules commonly used in AI
+module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
+module load Flask Seaborn OpenCV
+
+# ML Frameworks
+module load  PyTorch scikit-learn torchvision PyTorch-Lightning
+module load tensorboard
