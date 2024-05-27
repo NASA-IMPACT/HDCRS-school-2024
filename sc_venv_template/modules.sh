5f5dc3452949cb1b7bbd07d9ca7e22fcada7c128
xhagrg <grg.iksha@hotmail.com>
2024-05-27 12:33:45 -0500

    Fix torch versions and issues with missing files.

--- sc_venv_template/modules.sh
+++ sc_venv_template/modules.sh
@@ -6,5 +6,5 @@ module load mpi4py numba tqdm matplotlib IPython SciPy-Stack bokeh git
 module load Flask Seaborn OpenCV
 
 # ML Frameworks
-module load  PyTorch scikit-learn torchvision PyTorch-Lightning
+module load  scikit-learn
 module load tensorboard
